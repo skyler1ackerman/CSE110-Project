62f0c4472cead474b014f596ad6215fb666936e2
Skyler Ackerman <skyler1ackerman@gmail.com>
2020-10-22 23:02:17 -0700

    Insert userStoryBot

---
+++ userStoryBot.py
@@ -0,0 +1,43 @@
+import discord
+import os
+import userStore
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+userStories = []
+TOKEN = os.environ['DISCORD_TOKEN']
+GUILD= 'CSE110 Bot Testing'
+
+bot = commands.Bot(command_prefix='!')
+client = discord.Client()
+me = discord.ClientUser
+
+@bot.event
+async def on_ready():
+	print(f'{bot.user.name} has connected to Discord!')
+	userStories = userStore.loadStories()
+
+@bot.command(name='show')
+async def showCtx(ctx):
+	for x in userStories:
+		await ctx.send(x)
+	
+
+@bot.command(name='id')
+async def getId(ctx):
+	channel = ctx.channel
+	messages = await channel.history(limit=20).flatten()
+	for x in messages:
+		if "nerds" in x.content:
+			await ctx.send(x.author.name + " suggested " + x.content + " at " + str(x.created_at))
+			userStories.append(x.content)
+
+	
+@bot.command(name='s',help='Shuts down the script')
+async def shutdown(ctx):
+	await ctx.send('See ya nerds')
+	await ctx.bot.logout()
+	userStore.dumpStories(userStories)
+
+bot.run(TOKEN)
