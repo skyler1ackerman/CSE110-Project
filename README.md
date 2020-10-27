ba5989e727f6d07aba659172b22ba0f9b8354f2f
younseo <yoryu@ucsd.edu>
2020-10-26 23:01:24 -0700

    firebase setup and google signin completed. Filters out non-ucsd emails

---
+++ README.md
@@ -0,0 +1,5 @@
+# CSE110-Project
+
+This is a readme showing that I can push remotely. 
+
+I am making a change. This is the change. This is a second change. This is a THIRD change. You guessed it, a fourth change
---
+++ taskBot.py
@@ -0,0 +1,55 @@
+import os
+import taskStore
+
+import discord
+import random
+from dotenv import load_dotenv
+from discord.ext import commands
+from dateutil.parser import parse
+
+load_dotenv()
+TOKEN = os.environ['DISCORD_TOKEN']
+GUILD= 'CSE110 Bot Testing'
+
+allTask = {}
+# bot.p
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+
+# 2
+bot = commands.Bot(command_prefix='!')
+client = discord.Client()
+me = discord.ClientUser
+
+@bot.event
+async def on_ready():
+	print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name='new', help='Adds a new task to the list')
+async def newTask(ctx, name, *taskName):
+	allTask[name] = " ".join(taskName)
+	await ctx.send("Added task " + name)
+
+@bot.command(name='delete', help='Deletes a task from the list')
+async def newTask(ctx, name):
+	del allTask[name]
+	await ctx.send("Deleted task " + name)
+
+@bot.command(name='list', help='Lists all tasks')
+async def listTasks(ctx):
+	if allTask:
+		await ctx.send("These are the current tasks")
+		for item in allTask:
+			await ctx.send(item + ": " + allTask[item])
+	else:
+		await ctx.send("There are no current tasks")
+
+
+@bot.command(name='shutdown',help='Shuts down the script')
+async def shutdown(ctx):
+	await ctx.send('See ya nerds')
+	await ctx.bot.logout()
+	taskStore.dumpTasks(allTask)
+
+allTask = taskStore.loadTasks()
+bot.run(TOKEN)
