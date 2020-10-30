4bd9004d86ece144f3db18fdcf73a994ad37e085
Kaiwen Tan <kat066@ucsd.edu>
2020-10-29 17:06:29 -0700

    some texts

---
+++ src/components/MainPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const divStyle = {
+    textAlign: 'center'
+};
+
+class MainPage extends React.Component {
+    render(){
+        return(
+            <div style={divStyle}>
+
+                <style></style>
+                <head>Hello</head>
+                <h1>Triton Groups</h1>
+                <h2>A site that connects you with your classmates</h2>
+            </div>
+        );
+    }
+
+
+}
+
+
+export default MainPage;
