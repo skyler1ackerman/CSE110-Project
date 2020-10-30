960d3c66e63f000790171f28df17a1fcc58619e3
younseo <yoryu@ucsd.edu>
2020-10-30 00:49:23 -0700

    read file, convert to array completed

--- firebase/javascript/database.js
+++ firebase/javascript/database.js
@@ -1,15 +1,13 @@
 
-
-function dbFunction() {
-  console.log("dbFunction called :)"); 
-  firebase.initializeApp(firebaseConfig);
-  var arr = ['CSE110','CSE111','CSE110'];
-  populateClasses(arr);
-  // writeClasses('CSE110', 'Gary', 'Fall', '2020', 'test.com');
-  // writeClasses('CSE110', 'Gery', 'Fall', '2020', 'test2.com');
-  // writeClasses('CSE111', 'Gory', 'Fall', '2020', 'test.com');
-  // writeClasses('CSE112', 'Giry', 'Fall', '2020', 'test.com');
-}
+function dbFunction(arr) {
+    console.log("dbFunction called :)"); 
+    firebase.initializeApp(firebaseConfig);
+    populateClasses(arr);
+    // writeClasses('CSE110', 'Gary', 'Fall', '2020', 'test.com');
+    // writeClasses('CSE110', 'Gery', 'Fall', '2020', 'test2.com');
+    // writeClasses('CSE111', 'Gory', 'Fall', '2020', 'test.com');
+    // writeClasses('CSE112', 'Giry', 'Fall', '2020', 'test.com');
+  }
 
 function populateClasses(arr) {
   for(i = 0; i < arr.length; i++) {
@@ -28,3 +26,7 @@ function writeClasses(name, profname, qtr, yr, link) {
   });
 }
 
+
+function removeWhiteSpace(strwithspace){
+    return strwithspace.replace(/ /g,'');
+}
