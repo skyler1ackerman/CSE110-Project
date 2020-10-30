dd9200f3d9baaf1230669e555a8bb7e2642eae64
Amrita Kohli <amritakohli@live.com>
2020-10-30 00:10:21 -0700

    database skeleton

--- firebase/javascript/database.js
+++ firebase/javascript/database.js
@@ -1,3 +1,30 @@
+
+
 function dbFunction() {
-    console.log("dbFunction called :)");
+  console.log("dbFunction called :)"); 
+  firebase.initializeApp(firebaseConfig);
+  var arr = ['CSE110','CSE111','CSE110'];
+  populateClasses(arr);
+  // writeClasses('CSE110', 'Gary', 'Fall', '2020', 'test.com');
+  // writeClasses('CSE110', 'Gery', 'Fall', '2020', 'test2.com');
+  // writeClasses('CSE111', 'Gory', 'Fall', '2020', 'test.com');
+  // writeClasses('CSE112', 'Giry', 'Fall', '2020', 'test.com');
 }
+
+function populateClasses(arr) {
+  for(i = 0; i < arr.length; i++) {
+    writeClasses(arr[i], '', '', '', '')
+  }
+}
+function writeClasses(name, profname, qtr, yr, link) {
+
+  firebase.database().ref('classes/').child(name).push({
+    discordInfo: {
+      profName: profname,
+      quarter: qtr,
+      year: yr,
+      inviteURL: link,
+    }
+  });
+}
+
