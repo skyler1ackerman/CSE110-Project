98cfd29852b8516e9810c9d9eb502ff053bffb3a
Kaiwen Tan <kat066@ucsd.edu>
2020-10-29 17:54:34 -0700

    pic added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -14,6 +14,7 @@ class MainPage extends React.Component {
                 <head>Hello</head>
                 <h1>Triton Groups</h1>
                 <h2>A site that connects you with your classmates</h2>
+                <iframe src="https://drive.google.com/file/d/15hSrMKwayM1RAeHU9W-XG9P-MuyUNEID/preview" width="639" height="385"></iframe>
             </div>
         );
     }
