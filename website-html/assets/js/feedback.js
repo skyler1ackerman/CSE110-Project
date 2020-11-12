68a6bd4dc92c220fa60fcaf95c98e195306a5a37
Jennifer Purevsuren <jennifer@Jennifers-Air.attlocal.net>
2020-11-11 19:11:38 -0800

    feedback is connected to db

--- website-html/assets/js/feedback.js
+++ website-html/assets/js/feedback.js
@@ -1,8 +1,35 @@
+// function getFeedbackValues(){
+//     var a = document.getElementById("email").value;
+//     var b = document.getElementById("fullname").value;
+//     var c = document.getElementById("Issue_type").value;
+//     var d = document.getElementById("explanation").value;
+//     var msg = a.concat(b.concat(c.concat(d)));
+//     alert(msg);
+// }
 function getFeedbackValues(){
-    var a = document.getElementById("email").value;
-    var b = document.getElementById("fullname").value;
-    var c = document.getElementById("Issue_type").value;
-    var d = document.getElementById("explanation").value;
-    var msg = a.concat(b.concat(c.concat(d)));
-    alert(msg);
+    console.log("getFeedbackValues() called!");
+
+    if (!firebase.apps.length) {
+      console.log("firebase app initlized!");
+      firebase.initializeApp(firebaseConfig);
+    }
+  
+    // var feedback_ref = firebase.database().ref("Feedbacks/");
+    var counter = 1;
+    firebase.database().ref("Feedbacks/").on("value", function(snapshot) {
+      snapshot.forEach(function(snapshot) {
+        counter++;
+      });
+    });
+
+    var fbRef = "Feedbacks/";
+    var childRef =  "feedback" + counter + "/";
+    console.log(fbRef);
+  
+    firebase.database().ref(fbRef).child(childRef).set({
+        email: localStorage.getItem("user-email"),
+        fullname: localStorage.getItem("user-displayname"),
+        issue_type: document.getElementById("Issue_type").value,
+        explanation: document.getElementById("explanation").value,
+    });
 }
