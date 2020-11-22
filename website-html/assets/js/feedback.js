61a61b1acdc8a7ae263b98846b47662e5853b343
djuanit0x <dennisjuanito@gmail.com>
2020-11-21 18:38:11 -0800

    Implemented discord requests page on admin page (#39)

--- website-html/assets/js/feedback.js
+++ website-html/assets/js/feedback.js
@@ -38,7 +38,7 @@ function getFeedbackValues(){
 
 function getFeedbackOutside(){
   console.log("getFeedbackValues() called!");
-    
+
   var counter = 1;
   var fbRef = "Feedback/Outside";
   firebase.database().ref(fbRef).once("value").then(function (snapshot){
@@ -493,5 +493,3 @@ retrieveFeedbackUCSD()
 retrieveFeedbackOutside()
 retrieveResolvedOutside()
 retrieveResolvedUCSD()
-
-s
