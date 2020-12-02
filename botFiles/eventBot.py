38701dcc6056fb461cc94fe1f05df3401278c576
Al <aldr1234567@gmail.com>
2020-12-02 13:28:18 -0800

    eventbot 30 min reminder, extra err for delevent, shorted admin check

--- botFiles/eventBot.py
+++ botFiles/eventBot.py
@@ -30,15 +30,23 @@ async def on_ready():
 # newEvent will add a new event folder with data to firebase db given user input, for specific server folder
 @bot.command(name='newevent', aliases=['addevent','plan','setevent'], help="Adds a new event to the current server's schedule. Optional event name with command.")
 async def newEvent(ctx, *inputEventMsg):
+	# nested fuction, to use in secondary queries to only respond to same user
 	def verifyUser(newMessage):
 		return newMessage.author == ctx.author
+	# admin check, short circuit this with AND until final decision
+	if not ctx.message.author.guild_permissions.administrator and False:
+		msg = "You're not an admin {0.author.mention}, so you can't call this command".format(ctx.message)  
+		await ctx.send(msg)
+		return
+	# if user did not give any argument with command, query for event name
 	if len(inputEventMsg) == 0:
 		await ctx.send('Enter name of the event to be planned.')
 		inputEventMsg = await bot.wait_for("message", timeout=30, check=verifyUser)
 		eventName = inputEventMsg.content
+	# otherwise, turn tuple argument into one string
 	else:
 		eventName = ' '.join(inputEventMsg)
-
+	# if there's already an entry with this name for this server, end
 	if db.child(ctx.guild.id).child(FOLDER_STR).child(eventName).shallow().get().val():
 		await ctx.send('Event "' + eventName + '" already exists!')
 	else:
@@ -58,6 +66,7 @@ async def newEvent(ctx, *inputEventMsg):
 
 @newEvent.error
 async def newEventError(ctx, error):
+	# only give specific response for timeout, dont think anything else worth it
 	if isinstance(error.original, asyncio.exceptions.TimeoutError):
 		await ctx.send('Error: no response / time-out.')
 	else: 
@@ -68,18 +77,33 @@ async def newEventError(ctx, error):
 async def delEvent(ctx, *inputEventMsg):
 	def verifyUser(newMessage):
 		return newMessage.author == ctx.author
+	# admin check, short circuit this with AND until final decision
+	if not ctx.message.author.guild_permissions.administrator and False:
+		msg = "You're not an admin {0.author.mention}, so you can't call this command.".format(ctx.message)  
+		await ctx.send(msg)
+		return
+	# if no argument given, query for event name
 	if len(inputEventMsg) == 0:
 		await ctx.send('Enter eventName of the event to be removed.')
 		inputEventMsg = await bot.wait_for("message", timeout=30, check=verifyUser)
 		eventName = inputEventMsg.content
+	# else, turn tuple argument into one string
 	else:
 		eventName = ' '.join(inputEventMsg)
-		
+	# check if event name is used, and delete it
 	if db.child(ctx.guild.id).child(FOLDER_STR).child(eventName).shallow().get().val():
 		db.child(ctx.guild.id).child(FOLDER_STR).child(eventName).remove()
 		await ctx.send('Removed event "' + eventName + '".')
 	else:
 		await ctx.send('Event "' + eventName + '" does not exist!')
+
+@delEvent.error
+async def delEventError(ctx, error):
+	# only give specific response for timeout, dont think anything else worth it
+	if isinstance(error.original, asyncio.exceptions.TimeoutError):
+		await ctx.send('Error: no response / time-out.')
+	else: 
+		await ctx.send('Error: bad date or bad time entered.')
 
 # listEvents will access firebase db and list all events found for specific server
 @bot.command(name='schedule',aliases=['showevents','showschedule','showplan','showplanned','listevents'], help="Lists all events in the server's schedule.")
@@ -93,9 +117,14 @@ async def listEvents(ctx):
 	else:
 		await ctx.send("There are no events to list!")
 
-
+# comment this out for release?
 @bot.command(name='shutdown', help='Shuts down the bot across all servers.')
 async def shutdown(ctx):
+	# admin check, needed for shutdown but this impacts all servers
+	if not ctx.message.author.guild_permissions.administrator:
+		msg = "You're not an admin {0.author.mention}, so you can't use this command.".format(ctx.message)  
+		await ctx.send(msg)
+		return
 	await ctx.send('Shutting down ' + bot.user.name + "!")
 	await ctx.bot.logout()
 
@@ -111,7 +140,7 @@ async def checkSchedule():
 				guildAllEvents = db.get().val()[childGuild][FOLDER_STR]
 				# working null db check
 				if guildAllEvents is not None:
-					# get a datetime obj for current time to compare with events
+					# get a datetime obj for current time to compare with events, local time + tz naive
 					currDateTime = datetime.now()
 					# loop through all events, finish up started events and maybe remind
 					for eventFolder in guildAllEvents:
@@ -126,10 +155,16 @@ async def checkSchedule():
 							db.child(childGuild).child(FOLDER_STR).child(eventFolder).remove()
 						else:
 							# insert secondary reminder alerts here
-							dummy = 0
+							# 30 minute reminder, ex if event is 12:00 pm remind at (11:29:00 am, 11:30:00 am] w/ 60 second check interval
+							if (eventDateTime - timedelta(minutes=31)) < currDateTime <= (eventDateTime - timedelta(minutes=30)):
+								destinationChannel = bot.get_channel(guildAllEvents[eventFolder]['channel'])
+								if destinationChannel is not None:
+									await destinationChannel.send("@everyone, " + eventFolder + " starts in 30 minutes!")
 		# keep loop alseep for 60 seconds
 		await asyncio.sleep(60)
 
+botStartTime = datetime.now()
+print(botStartTime)
 # main, note that checkSchedule() will run before bot start-up and old events will be deleted with no message 
 bot.loop.create_task(checkSchedule())
 botStarted = True
