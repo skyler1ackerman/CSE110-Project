be60d502dbecdb870f937acad47012740079390c
younseo <yoryu@ucsd.edu>
2020-12-02 04:29:02 -0800

    Add everything besides matching

--- website-html/assets/js/livechat.js
+++ website-html/assets/js/livechat.js
@@ -1,110 +1,154 @@
-function closeChatRoom(){
-    console.log("close called");
-    document.getElementById("chatroom").style.visibility = "hidden";
-    document.getElementById("openChatroom").style.visibility = "visible";
-}
+function instantiateSession(otherID, otherName, otherEmail, otherPhotoUrl, otherWelcomeMsg){
+    (function(t,a,l,k,j,s){
+        s=a.createElement('script');s.async=1;s.src="https://cdn.talkjs.com/talk.js";
+        a.head.appendChild(s);
+        k=t.Promise;
+        t.Talk={v:3,ready:{then:function(f){if(k)return new k(function(r,e){l.push([f,r,e])});
+            l.push([f])},catch:function(){
+                return k&&new k()
+            },c:l}
+        };
+    })(window,document,[]);
 
-function openChatRoom(){
-    console.log("close called");
-    document.getElementById("openChatroom").style.visibility = "hidden";
-    document.getElementById("").style.visibility = "visible";
-    // addOnlineNumberAsUserSignin();
-}
 
-
-function addOnlineNumberAsUserSignin(){
-	var numOnlinePpl = (document.getElementById("numonline").innerHTML) || 0 ;
-	numOnlinePpl++;
-	firebase.database().ref('messages/').update({
-		onlineCount: numOnlinePpl,
- 	});
-}
-
-function sendMessage() {
-	// get message
-	var message = document.getElementById("message").value;
-	// save in database
-	firebase.database().ref("messages/dataID").push().set({
-		"sender": localStorage.getItem("user-displayname"),
-		"message": message
+    Talk.ready.then(function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log("inside user if statement")
+                var me = new Talk.User({
+                    id: user.uid,
+                    name: user.displayName,
+                    email: user.email,
+                    photoUrl: user.photoURL,
+                    welcomeMessage: "Hi! I am ".concat(user.displayName),
+                });
+                window.talkSession = new Talk.Session({
+                    appId: "tGPhd31E",
+                    me: me     
+                });
+                var other = new Talk.User({
+                    id: otherID, 
+                    name: otherName,
+                    email: otherEmail,
+                    photoUrl: otherPhotoUrl,
+                    welcomeMessage: otherWelcomeMsg
+                });
+        
+                var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other))
+                conversation.setParticipant(me);
+                conversation.setParticipant(other);
+        
+                var inbox = talkSession.createInbox({selected: conversation});
+                inbox.mount(document.getElementById("talkjs-container"));
+            } else {
+              // No user is signed in.
+            }
+        });
     });
-    document.getElementById("message").value = "";
-	// prevent form from submitting
-	return false;
 }
 
-// listen for incoming messages
-firebase.database().ref("messages/dataID").on("child_added", function (snapshot) {
-    var html = "";
-    // give each message a unique ID
-    // html += "<li style=\"text-align: right; margin-right:5px;\" id='message-" + snapshot.key + "'>";
-    // show delete button if message is sent by me
-    if (snapshot.val().sender == localStorage.getItem("user-displayname")) {
-        html += "<li style=\"text-align: right; margin:8px 0px 8px 0px;\" id='message-" + snapshot.key + "'>";
-        html += "<button style=\"color:darkred;\" data-id='" + snapshot.key + "' onclick='deleteMessage(this);' class=\"no-style\">";
-            html += "&times;"
-        html += "</button>";
-        html += "     <span style='background-color:#b19cd9; border-radius:10px; padding:2px 4px 2px 4px;'>" + snapshot.val().message + "</span>";
+function match(){
+    //get user info
+    var user = firebase.auth().currentUser;
+    var id, name, email, photoUrl, welcomeMessage; 
+    if (user != null) {
+        id = user.uid;
+        console.log("user uid:", id);
+        name = user.displayName;
+        console.log("user name:", name);
+        email = user.email;
+        console.log("user email:", email);
+        photoUrl = user.photoURL;
+        console.log("user photoUrl:", photoUrl);
+        welcomeMessage = "Hi! I am ".concat(name);
+        console.log("user welcome msg:", welcomeMessage);
     }
-    else{
-        //this for changing text color for each person. user color hex code.
-        var senderStr = String(snapshot.val().sender);
-        var asciiSum = 0;
-        for (var i = 0; i < senderStr.length; i++) {
-            asciiSum += senderStr.charCodeAt(i)*i*82;
-        }
-        var colorHexCode = (asciiSum%16777215).toString(16);
-        if(colorHexCode.length == 6){
-            colorHexCode = colorHexCode.concat("");
-        } else if(colorHexCode.length == 5){
-            colorHexCode = colorHexCode.concat("F");
-        } else if(colorHexCode.length == 4){
-            colorHexCode = colorHexCode.concat("3F");
-        }else if(colorHexCode.length == 3){
-            colorHexCode = colorHexCode.concat("3EF");
-        }else if(colorHexCode.length == 2){
-            colorHexCode = colorHexCode.concat("F3EF");
-        } else if(colorHexCode.length == 1){
-            colorHexCode = colorHexCode.concat("FF3EF");
-        }
-        html += "<li style=\"text-align: left; margin-left:0px;\" id='message-" + snapshot.key + "'>";
-        html += "<span style='font-size:20px; color:#" + colorHexCode +  ";'>&#9632;</span>"; 
-        html += "<span style='font-size:12px; color:white; background-color:#"+ colorHexCode +"; border-radius:3px;'>"+String(snapshot.val().sender).split(" ")[0] +"</span>: <span style='padding:2px;'>" + snapshot.val().message + "</span>";
-    }
-    if (snapshot.val().sender != localStorage.getItem("user-displayname")) {
-    html += "</li>";
-    }
-    document.getElementById("messages").innerHTML += html;
-    var objDiv = document.getElementById("messagesContainer");
-    objDiv.scrollTop = objDiv.scrollHeight;
-});
 
-//listen to how many ppl are online
-firebase.database().ref("messages/onlineCount").on('value', function (snapshot) {
-    document.getElementById("numonline").innerHTML = snapshot.val();
-    console.log("numlogin!: ",snapshot.val());
-});
-
-
-
-function deleteMessage(self) {
-    // get message ID
-    var txt;
-    var res = confirm("Remove the message?");
-    if (res == true) {
-        var messageId = self.getAttribute("data-id");
-        // delete message
-        firebase.database().ref("messages/dataID").child(messageId).remove();
-    } else {
-        //nothing happens
-    }
+    firebase.database().ref("livechat/queues").on('value', (snapshot) =>{
+        // console.log(snapshot.val()); //snapshot of queues
+        snapshot.forEach(function(childSnapshot) {
+            // if(childSnapshot.child("name").val()==Name){
+            //     var name = childSnapshot.child('name').val();
+            //     var claimnNumber = childSnapshot.child('claimnNumber').val();
+            //     var phoneNumber = childSnapshot.child('phoneNumber').val();                
+            // }  
+            
+            console.log(childSnapshot.val()); //snapshot of participants
+            console.log(childSnapshot.child("participants").numChildren()); //get number of participants in each queue
+            childSnapshot.forEach(function(grandChildSnapshot){
+                console.log(grandChildSnapshot.val()); //snapshot of each user's info
+                // console.log(grandChildSnapshot.numChildren());
+            });
+        }); 
+        
+    });
     
+    // firebase.database().ref("livechat/queues").push().set({
+    //     "participants": {
+    //         [id]: {
+    //           "id": id,
+    //           "name": name,
+    //           "email": email,
+    //           "photoUrl": photoUrl,
+    //           "welcomeMessage": welcomeMessage,
+    //         },
+    //     },
+    // });
 }
- 
-// attach listener for delete message
-firebase.database().ref("messages/dataID").on("child_removed", function (snapshot) {
-    // remove message node
-    document.getElementById("message-" + snapshot.key).innerHTML = "*message removed*";
-});
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+const newMatch = async () => {
+    if (typeof talkSession !== 'undefined') {
+        disconnect();
+    }
+    document.getElementById("chatGuide").innerHTML = "Loading Chat...";
+    
+    // do a bunch of stuff here regarding matching
+
+    instantiateSession("Sebastian", "Sebastian", "Sebastian@example.com", "https://demo.talkjs.com/img/sebastian.jpg","Hey, how can I help?");
+    await delay(10000);
+    document.getElementById("chatGuide").innerHTML = "";
+}
+function disconnect(){
+    talkSession.destroy();
+}
+
+// //possible scenarios
+// When user clicks "Start New Match" button, disconnect the chat room, take a snapshot of "livechat/queue"
+// 1. When all queues are full or when there is no queue
+//         create new queue AND listen if anyone joins into your queue
+//             if no one joins, wait
+//             if anyone joins, match
+// 2. When there is a queue with a spot left
+//         join existing queue AND match
+            
+// //list of functions
+// check if all queues are full or empty()
+// join existing queue()
+// create new queue and listen to the newly made queue()
+// match() <-- instantiateSession()
 
 
+
+
+
+function postNewClaim(){
+    $.when($.ajax(getNewClaimInput())).then(function () {
+        callFBset();
+        uploadAlert();
+    });
+}
+
+function getClaimByName(Name){
+    firebase.database().ref('claims/dataID').once('value').then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            if(childSnapshot.child("name").val()==Name){
+                var name = childSnapshot.child('name').val();
+                var claimnNumber = childSnapshot.child('claimnNumber').val();
+                var phoneNumber = childSnapshot.child('phoneNumber').val();                
+            }  
+        }); 
+    });
+}
+
+// instantiateSession();
