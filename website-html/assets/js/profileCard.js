53b3d76e9302837eef9394f5da849fa2641d59c1
jennifersp <44716627+jennifersp@users.noreply.github.com>
2020-12-05 00:59:26 -0800

    updated Admin Dashboard button (#137)
    * profile popout in classDB
    
    * add younseo hand to pic
    
    * link abstraction
    
    * commented admin button
    
    * relocated Admin Dashboard button
    
    Co-authored-by: Al <aldr1234567@gmail.com>

--- website-html/assets/js/profileCard.js
+++ website-html/assets/js/profileCard.js
@@ -5,3 +5,21 @@ function goToContactUs(){
 function goToAboutUs(){
     window.location.href = "aboutUs.html";
 }
+
+function ifAdminDashboard() {
+    // kaiwen's jquery thing to make a new button on page for admin, change if wanted
+    firebase.database().ref("AdminUser/").once("value").then(function(snapshot){
+        snapshot.forEach(function (childSnapshot){
+            if(localStorage.getItem("user-email")==childSnapshot.val().email){
+                document.getElementById("directAdmin").style.display = "inline-block";
+            }
+        });
+    });
+}
+
+
+function goToAdminDashboard(){
+    window.location.href = "admin-dashboard.html";
+}
+
+ifAdminDashboard();
