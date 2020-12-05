e96cde924ff264b3ceee39c7c3a9928c3a8feb67
aabroukh <63086438+aabroukh@users.noreply.github.com>
2020-12-05 02:25:18 -0800

    Add files via upload

--- website-html/assets/js/add-Community.js
+++ website-html/assets/js/add-Community.js
@@ -1,17 +1,7 @@
-/*function writeNewCommunityData() {
-    // TODO: check the child tree (do the names match?)
-    firebase.database().ref("Clubs/Category").set({
-        comName: document.getElementById("serverName").value,
-        discordLink: document.getElementById("discordLink").value,
-        type: document.getElementById("type").value,
-        category: document.getElementById("comCategory").value,
-        email: document.getElementById("email").value,
-        description: document.getElementById("description").value
-    });
-}*/
+var clubsArr = []
 
 // Get all the 
-/*function getClubSnapshot() {
+function getClubSnapshot() {
     console.log("getClassSnapshot() called :)");
     // TODO: check the child tree (do the names match?)
     var ref = firebase.database().ref("clubs");
@@ -21,41 +11,27 @@
             clubsArr.push(clubName);
         });
     });
-}*/
+}
 
 // we want to check if the community already exists in the database
 // we do this by comparing community name
 async function submit_community() {
-/*    if (!clubsArr){
+    if (!clubsArr){
         getClubSnapshot();
     }
     var serverName = document.getElementById("serverName").value;
+    var category = document.getElementById("comCategory").value;
 
     if (clubsArr.includes(serverName)){
         alert("The community already exists. Try using the search communities page :)");
-    } else {*/
-    	/*var serverName = document.getElementById("serverName").value;
-        var categoryName = document.getElementById("comCategory").value;
-        var link = document.getElementById("discordLink").value;
-        var type = document.getElementById("type").value;
-        var email = document.getElementById("email").value;
-        var desc = document.getElementById("description").value;
-
-        firebase.database().ref('clubs/'+categoryName+'/'+serverName).update({
-            inviteLink: link,
-            org_type: type,
-            contact: email,
-            description: desc,
-        });
-    }*/
-	firebase.database().ref('clubTemp/').child(document.getElementById("comCategory").value).child(document.getElementById("serverName").value).update({
-		inviteLink: document.getElementById("discordLink").value,
+    } else {
+        firebase.database().ref('clubs').child(category).child(serverName).set({
+        inviteLink: document.getElementById("discordLink").value,
         org_type: document.getElementById("type").value,
         contact: document.getElementById("email").value,
         description: document.getElementById("description").value,
         status: "",
         social_media: ""
-  	});
+        });
+    }
 }
-
-//var clubsArr = []
