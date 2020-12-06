cc37daf7c26306ad1ae5265323846cd10168585b
Jennifer Purevsuren <44716627+jennifersp@users.noreply.github.com>
2020-12-06 03:24:00 -0800

    favicon fix

--- website-html/assets/js/favIcon.js
+++ website-html/assets/js/favIcon.js
@@ -1,16 +1,21 @@
-lightSchemeIcon = document.querySelector('link#light-scheme-icon');
-darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+// lightSchemeIcon = document.querySelector("link[id=light-scheme-icon]");
+// darkSchemeIcon = document.querySelector("link[id=dark-scheme-icon]");
+// OR can be grabbed like ^
+lightSchemeIcon = document.querySelector("link#light-scheme-icon");
+darkSchemeIcon = document.querySelector("link#dark-scheme-icon");
 
-function onUpdate() {
+async function onUpdate() {
+  console.log(darkSchemeIcon);
   console.log("onupdate  called");
   if (matcher.matches) {
+    console.log("one called");
     lightSchemeIcon.remove();
     document.head.append(darkSchemeIcon);
-    console.log("one called");
   } else {
-    document.head.append(lightSchemeIcon);
-    darkSchemeIcon.remove();
     console.log("two called");
+    document.head.append(lightSchemeIcon);
+    console.log(darkSchemeIcon);
+    darkSchemeIcon.remove();
   }
 }
 
