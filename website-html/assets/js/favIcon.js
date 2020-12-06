e062a57df697af6707cf27d1140eacfba1dc4027
Jennifer Purevsuren <44716627+jennifersp@users.noreply.github.com>
2020-12-06 03:30:09 -0800

    prev favicon errors fixed

--- website-html/assets/js/favIcon.js
+++ website-html/assets/js/favIcon.js
@@ -5,14 +5,10 @@ lightSchemeIcon = document.querySelector("link#light-scheme-icon");
 darkSchemeIcon = document.querySelector("link#dark-scheme-icon");
 
 async function onUpdate() {
-  console.log(darkSchemeIcon);
-  console.log("onupdate  called");
   if (matcher.matches) {
-    console.log("one called");
     lightSchemeIcon.remove();
     document.head.append(darkSchemeIcon);
   } else {
-    console.log("two called");
     document.head.append(lightSchemeIcon);
     console.log(darkSchemeIcon);
     darkSchemeIcon.remove();
