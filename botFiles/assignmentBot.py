d5b0681b4abb21e61e48074725192baa50fdf147
skyler1ackerman <51422510+skyler1ackerman@users.noreply.github.com>
2020-12-07 02:00:53 -0800

    Sky fancy merge bots (#162)
    * Fixed up ruleBot to work with new db standards
    
    * Added defaultRules to upstream
    
    * Updated DB schema for the assignment bot.
    
    * Put everything in cogs now. Other than that should be same functionality.
    
    * Fixed noneCheck
    
    * Fixed timing
    
    Co-authored-by: Skyler Ackerman <skyler1ackerman@gmail.com>

--- botFiles/assignmentBot.py
+++ botFiles/assignmentBot.py
@@ -1,124 +1,94 @@
 import discord
-import os
 import pyrebase
 import re
 import asyncio
 from config import fireConfig
-from config import token1
-from dotenv import load_dotenv
-from discord.ext import commands, tasks
+from discord.ext import commands
 from datetime import datetime
-
-#used skylers skeleton
-def make_datetime(lst):
-    date_str = lst[1]
-    return datetime.strptime(date_str, '%m/%d/%Y')
+from collections import OrderedDict
 
 firebase = pyrebase.initialize_app(fireConfig)
-auth = firebase.auth()
 db = firebase.database()
-
-load_dotenv()
-TOKEN = token1
-GUILD= 'CSE110 Bot Testing'
-
-bot = commands.Bot(command_prefix='!')
-client = discord.Client()
-
 top = "Assignments"
 
-@bot.event
-async def on_ready():
-	print(f'{bot.user.name} has connected to Discord!')
+def setup(bot):
+    bot.add_cog(AssignmentBot(bot))
 
-#!addAssign "assignment name" mm/dd/yyyy
-@bot.command(name='addAssign', help='Adds a new assignment to the list')
-async def newEvent(ctx, name=None, *date):
-	content = ctx.message.content
-	#check for proper name format
-	if name is not None and (content.count('\"') == 2):
-		title = content.split('"')[1].strip()
-		date1 = content[content.rindex('"')+1:].strip()
-		#strip out mm/dd/yyyy format from following 
-		dateFound = re.search('(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}' , date1)
-		#check date format, if date at all, if empty quotes
-		if dateFound and title:
-			#check if assignment name already exists in db
-			if db.child(ctx.channel.id).child(top).child(title).shallow().get().val():
-				await ctx.send('Assignment "' + name + ' already exists!')
+class AssignmentBot(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	#!addAssign "assignment name" mm/dd/yyyy
+	@commands.command(name='addAssign', help='Adds a new assignment to the list')
+	async def newEvent(self, ctx, name=None, *date):
+		content = ctx.message.content
+		#check for proper name format
+		if name and (content.count('\"') == 2):
+			title = content.split('"')[1].strip()
+			#strip out mm/dd/yyyy format from following 
+			dateFound = re.search('(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}' , content[content.rindex('"')+1:].strip())
+			#check date format, if date at all, if empty quotes
+			if dateFound and title:
+				#check if assignment name already exists in db
+				if db.child(ctx.guild.id).child(top).child(title).shallow().get().val():
+					await ctx.send('Assignment "' + name + '" already exists!')
+				else:
+					db.child(ctx.guild.id).child(top).child(title).child('title').set(title)
+					db.child(ctx.guild.id).child(top).child(title).child('date').set(''.join(str(dateFound.group())))
+					if(len(db.child(ctx.guild.id).child(top).get().val()) == 1):
+						self.bot.loop.create_task(self.checkSchedule(ctx.guild))
+					await ctx.send('Added assignment "' + name + '"')
 			else:
-				db.child(ctx.channel.id).child(top).child(title).set(''.join(str(dateFound.group())))
-				await ctx.send('Added assignment "' + name + '"')
+				await ctx.send('Please use the format: !addAssign "Assignment Name" mm/dd/yyyy')
 		else:
 			await ctx.send('Please use the format: !addAssign "Assignment Name" mm/dd/yyyy')
-	else:
-		await ctx.send('Please use the format: !addAssign "Assignment Name" mm/dd/yyyy')
 
-#!delAssign "assignment name"
-@bot.command(name='delAssign', help='Deletes an assignment from the list')
-async def delEvent(ctx, name=None):
-	content = ctx.message.content
-	#check empty argument and in quotes
-	if name is not None and (content.count('\"') == 2):
-		titleDel = content.split('"')[1].strip()
-		#check empty quotes
-		if titleDel:
-			#check exists then removes from db
-			if db.child(ctx.channel.id).child(top).child(titleDel).shallow().get().val():
-				db.child(ctx.channel.id).child(top).child(titleDel).remove()
-				await ctx.send('Removed assignment "' + titleDel + '"')
+	#!delAssign "assignment name"
+	@commands.command(name='delAssign', help='Deletes an assignment from the list')
+	async def delEvent(self, ctx, name=None):
+		content = ctx.message.content
+		#check empty argument and in quotes
+		if name and (content.count('\"') == 2):
+			titleDel = content.split('"')[1].strip()
+			#check empty quotes
+			if titleDel:
+				#check exists then removes from db
+				if db.child(ctx.guild.id).child(top).child(titleDel).shallow().get().val():
+					db.child(ctx.guild.id).child(top).child(titleDel).remove()
+					await ctx.send('Removed assignment "' + titleDel + '"')
+				else:
+					await ctx.send('Assignment "' + name + '" does not exist')
 			else:
-				await ctx.send('Assignment "' + name + '" does not exist')
+				await ctx.send('Please use the format: !delAssign "Assignment Name"')
 		else:
-			await ctx.send('Please use the format: !delAssign "Assignment Name"')
-	else:
-			await ctx.send('Please use the format: !delAssign "Assignment Name"')
+				await ctx.send('Please use the format: !delAssign "Assignment Name"')
 
-#!listAssign
-@bot.command(name='listAssign', help='Lists all assignments')
-async def listEvents(ctx):
-	if db.child(ctx.channel.id).child(top).shallow().get():
-		newDate = {}
-		assignArray = []
-		allAssign = db.child(ctx.channel.id).child(top).get().val()
-		for channelID in db.get().val():
-			if int(channelID) == ctx.channel.id:
-				for assignment in db.get().val()[channelID]['Assignments']:
-					newDate[assignment] = db.get().val()[channelID]['Assignments'][assignment]
-				newData = list(newDate.items())
-				sortedData = sorted(newData, key=make_datetime)
-				for x, y in sortedData:
-					await ctx.send(y+': '+x)
-	else:
-		await ctx.send("There are no assignments to list!")
+	#!listAssign
+	@commands.command(name='listAssign', help='Lists all assignments')
+	async def listEvents(self, ctx):
+		if db.child(ctx.guild.id).child(top).get().val():
+			assignDict = dict(db.child(ctx.guild.id).child(top).get().val())
+			assignDict = dict(sorted(assignDict.items(), key=lambda k_v: datetime.strptime(k_v[1]['date'], '%m/%d/%Y')))
+			for assKey in assignDict:
+				await ctx.send(assignDict[assKey]['title'] + ' due on ' + assignDict[assKey]['date'])
+		else:
+			await ctx.send('There are no assignments to list!')
 
 
-@bot.command(name='s',help='Shuts down the script')
-async def shutdown(ctx):
-	await ctx.send('Shutting down AssignmentBot')
-	await ctx.bot.logout()
-
-async def checkSchedule():
-	while(True):
-		# sanity check
-		if db.get().val() is not None:
-			for channelID in db.get().val():
-				for assignment in db.get().val()[channelID]['Assignments']:
-					date = datetime.strptime(db.get().val()[channelID]['Assignments'][assignment], '%m/%d/%Y')
+	async def checkSchedule(self, guild):
+		while(True):
+			# sanity check
+			if db.child(guild.id).child(top).get().val():
+				assignDict = dict(db.child(guild.id).child(top).get().val())
+				for assKey in assignDict:
+					date = datetime.strptime(assignDict[assKey]['date'], '%m/%d/%Y')
 					currDateTime = datetime.today()
+					channel = discord.utils.get(guild.text_channels, name="general")
 					if date.date() == currDateTime.date():
-						channel = bot.get_channel(int(channelID))
 						if channel:
-							await channel.send("@everyone, " + assignment + " is due today.")
+							await channel.send("@everyone, " + assignDict[assKey]['title'] + " is due today.")
 					elif date.date() < currDateTime.date():
-						channel = bot.get_channel(int(channelID))
 						if channel:
-							await channel.send("@everyone, " + assignment + " was due.")
-							db.child(channelID).child(top).child(assignment).remove()
-
-#		# keep loop asleep for 8 hours
-		await asyncio.sleep(28800)
-## main, note that checkSchedule() will run before bot start-up and old events will be deleted with no message 
-bot.loop.create_task(checkSchedule())
-
-bot.run(TOKEN)
+							await channel.send("@everyone, " + assignDict[assKey]['title'] + " was due.")
+							db.child(guild.id).child(top).child(assKey).remove()
+			await asyncio.sleep(28800)
--- botFiles/reportBot.py
+++ botFiles/reportBot.py
@@ -1,118 +1,105 @@
 import discord
 import asyncio
 import datetime
-# You can replace this with your own token in the config file
-from config import DISCORD_TOKEN as TOKEN
-from dotenv import load_dotenv
 from discord.ext import commands
 
-# I set the intents to all so I can listen for reactions
-# If I were less lazy I would find the exact intents command
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+def setup(bot):
+    bot.add_cog(ReportBot(bot))
 
-# This function gets called when the admin approves deleting a reported message
-async def delMsg(susMsg, reportMsg, reportMsgList):
-	# We do a try except loop to avoid the edge case where another mod already deleted the given message
-	try:
-		# Tell the admin that we received the command to delete, then delete the message
-		await reportMsg.channel.send('Deleting message "' + susMsg.content + '"')
-		await susMsg.delete()
-	# If someone already manually deleted the message we would hit an error. This is just to avoid that.
-	except discord.errors.NotFound:
-		await reportMsg.channel.send('That message has already been deleted!')
-	# Send a message to every admin besides the one that deleted the message letting them know it got deleted.
-	for channel in [msg.channel for msg in reportMsgList if msg.channel.recipient != reportMsg.channel.recipient]:
-		await channel.send(str(reportMsg.channel.recipient) + ' deleted message "' + susMsg.content + '"')
+class ReportBot(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
 
-# This function gets called when the admin rejects deleting a reported message
-async def keepMsg(susMsg, reportMsg, reportMsgList):
-	# We do a try except loop to avoid the edge case where another mod already deleted the given message
-	try:
-		# Tell the admin that we received the command to allow the message
-		await reportMsg.channel.send('Ignoring message "' + susMsg.content + '"')
-	# If someone already manually deleted the message we would hit an error. This is just to avoid that.
-	except discord.errors.NotFound:
-		await reportMsg.channel.send('That message has already been deleted!')
-	# Send a message to every admin besides the one that allowed the message letting them know it was ignored.
-	for channel in [msg.channel for msg in reportMsgList if msg.channel.recipient != reportMsg.channel.recipient]:
-		await channel.send(str(reportMsg.channel.recipient) + ' ignored message "' + susMsg.content + '"')
-
-# This function is called after the report is sent to the admin
-# It waits for the admin to react with a check or x
-async def waitGen(reportMsgList, susMsg):
-	# The admin can react with a check to delete the message, or an x to keep the message
-	optionDict = {'✅':delMsg,'❌':keepMsg}
-	# Checks that the admin reacted with one of the two options
-	def check(react, user_):
-		# We check that A. They reacted with one of the two emojis
-		# B. The reaction was on the correct report message
-		# C. There are two reactions (The bot's and the admins)
-		return str(react.emoji) in optionDict.keys() and react.message.id in [msg.id for msg in reportMsgList] and react.count == 2
-	# The bot will wait for the admin to react to the report message until the timeout
-	# The timeout can be removed if we want it to wait forever, but I advise against that
-	try:
-		reportReact, reportAdmin = await bot.wait_for('reaction_add', timeout=60, check=check)
-	# If the wait times out, simply continue
-	except asyncio.TimeoutError:
-		pass
-	# If the wait returns true, then run the function corresponding to the given emoji
-	else:
-		await optionDict[reportReact.emoji](susMsg, reportReact.message, reportMsgList)
-
-# This function is called when a user 'flags' a message, AKA reacts with a red square
-async def sendReport(reaction, user):
-	# The flagged message
-	susMsg = reaction.message
-	# Immediately remove the flag so that not everyone on the server can see who flagged
-	await reaction.remove(user)
-	# Get a list of all of the admins in the guild
-	adminsList = [admin for admin in susMsg.guild.members if admin.guild_permissions.administrator and not admin.bot]
-	# Loop through all the admins
-	reportMsgList = []
-	for admin in adminsList:
-		# Send a report to the admin including who reported who, the message, the time, and instructions
+	# This function gets called when the admin approves deleting a reported message
+	async def delMsg(self, susMsg, reportMsg, reportMsgList):
+		# We do a try except loop to avoid the edge case where another mod already deleted the given message
 		try:
-			# If there isn't already a dm_channel between the bot and the user, create one
-			if not admin.dm_channel:
-				await admin.create_dm()
-			reportMsg = await admin.dm_channel.send(user.name + ' reported ' + susMsg.author.name + 
-			' for the following message:\n"' + susMsg.content + '"\nSent ' + 
-			(susMsg.created_at + datetime.timedelta(hours=4)).strftime("%A, %B %d at %H:%M") +
-			'\nPlease react with a ✅ to delete the reported message, or a ❌ to do nothing')
-			# Prepopulate the message with the two options. This makes it easier for the admin to remove/keep a message
-			await reportMsg.add_reaction('✅')
-			await reportMsg.add_reaction('❌')
-			# Make a list of all of the messages that went out to admins
-			reportMsgList.append(reportMsg)
-		except (discord.errors.Forbidden,discord.errors.HTTPException):
-			print('Skipping user', admin.name)
+			# Tell the admin that we received the command to delete, then delete the message
+			await reportMsg.channel.send('Deleting message "' + susMsg.content + '"')
+			await susMsg.delete()
+		# If someone already manually deleted the message we would hit an error. This is just to avoid that.
+		except discord.errors.NotFound:
+			await reportMsg.channel.send('That message has already been deleted!')
+		# Send a message to every admin besides the one that deleted the message letting them know it got deleted.
+		for channel in [msg.channel for msg in reportMsgList if msg.channel.recipient != reportMsg.channel.recipient]:
+			await channel.send(str(reportMsg.channel.recipient) + ' deleted message "' + susMsg.content + '"')
+
+	# This function gets called when the admin rejects deleting a reported message
+	async def keepMsg(self, susMsg, reportMsg, reportMsgList):
+		# We do a try except loop to avoid the edge case where another mod already deleted the given message
+		try:
+			# Tell the admin that we received the command to allow the message
+			await reportMsg.channel.send('Ignoring message "' + susMsg.content + '"')
+		# If someone already manually deleted the message we would hit an error. This is just to avoid that.
+		except discord.errors.NotFound:
+			await reportMsg.channel.send('That message has already been deleted!')
+		# Send a message to every admin besides the one that allowed the message letting them know it was ignored.
+		for channel in [msg.channel for msg in reportMsgList if msg.channel.recipient != reportMsg.channel.recipient]:
+			await channel.send(str(reportMsg.channel.recipient) + ' ignored message "' + susMsg.content + '"')
+
+	# This function is called after the report is sent to the admin
+	# It waits for the admin to react with a check or x
+	async def waitGen(self, reportMsgList, susMsg):
+		# The admin can react with a check to delete the message, or an x to keep the message
+		optionDict = {'✅':self.delMsg,'❌':self.keepMsg}
+		# Checks that the admin reacted with one of the two options
+		def check(react, user_):
+			# We check that A. They reacted with one of the two emojis
+			# B. The reaction was on the correct report message
+			# C. There are two reactions (The bot's and the admins)
+			return str(react.emoji) in optionDict.keys() and react.message.id in [msg.id for msg in reportMsgList] and react.count == 2
+		# The bot will wait for the admin to react to the report message until the timeout
+		# The timeout can be removed if we want it to wait forever, but I advise against that
+		try:
+			reportReact, reportAdmin = await self.bot.wait_for('reaction_add', timeout=60, check=check)
+		# If the wait times out, simply continue
+		except asyncio.TimeoutError:
 			pass
-		# Wait for the admin report 
-	await waitGen(reportMsgList, susMsg)
+		# If the wait returns true, then run the function corresponding to the given emoji
+		else:
+			await optionDict[reportReact.emoji](susMsg, reportReact.message, reportMsgList)
 
-# Let's me know that the bot is ready
-@bot.event
-async def on_ready():
-	print(f'{bot.user.name} has connected to Discord!')
+	# This function is called when a user 'flags' a message, AKA reacts with a red square
+	async def sendReport(self, reaction, user):
+		# The flagged message
+		susMsg = reaction.message
+		# Immediately remove the flag so that not everyone on the server can see who flagged
+		await reaction.remove(user)
+		# Get a list of all of the admins in the guild
+		adminsList = [admin for admin in susMsg.guild.members if admin.guild_permissions.administrator and not admin.bot]
+		# Loop through all the admins
+		reportMsgList = []
+		for admin in adminsList:
+			# Send a report to the admin including who reported who, the message, the time, and instructions
+			try:
+				# If there isn't already a dm_channel between the bot and the user, create one
+				if not admin.dm_channel:
+					await admin.create_dm()
+				reportMsg = await admin.dm_channel.send(user.name + ' reported ' + susMsg.author.name + 
+				' for the following message:\n"' + susMsg.content + '"\nSent ' + 
+				(susMsg.created_at + datetime.timedelta(hours=4)).strftime("%A, %B %d at %H:%M") +
+				'\nPlease react with a ✅ to delete the reported message, or a ❌ to do nothing')
+				# Prepopulate the message with the two options. This makes it easier for the admin to remove/keep a message
+				await reportMsg.add_reaction('✅')
+				await reportMsg.add_reaction('❌')
+				# Make a list of all of the messages that went out to admins
+				reportMsgList.append(reportMsg)
+			except (discord.errors.Forbidden,discord.errors.HTTPException):
+				print('Skipping user', admin.name)
+				pass
+			# Wait for the admin report 
+		await self.waitGen(reportMsgList, susMsg)
 
-# Runs whenever a new reaction is added
-@bot.event
-async def on_reaction_add(reaction, user):
-	# This is a dictionary of all of the emojis that the bot checks for.
-	# Right now it's just the red flag
-	optionDict = {'🟥': sendReport}
-	# If the emoji sent is in the dict.keys(), it will run that function
-	try:
-		await optionDict[reaction.emoji](reaction, user)
-	# Otherwise, it will ignore it
-	except KeyError:
-		pass
 
-# Regular shut down of the bot
-@bot.command(name='s',help='Shuts down the script')
-async def shutdown(ctx):
-	await ctx.send('Shutting down ReportBot')
-	await ctx.bot.logout()
-# Run the bot
-bot.run(TOKEN)
+	# Runs whenever a new reaction is added
+	@commands.Cog.listener()
+	async def on_reaction_add(self, reaction, user):
+		# This is a dictionary of all of the emojis that the bot checks for.
+		# Right now it's just the red flag
+		optionDict = {'🟥': self.sendReport}
+		# If the emoji sent is in the dict.keys(), it will run that function
+		try:
+			await optionDict[reaction.emoji](reaction, user)
+		# Otherwise, it will ignore it
+		except KeyError:
+			pass
