bda91b36fb07618319d562a089a33a83718dd52d
Skyler Ackerman <skyler1ackerman@gmail.com>
2020-12-07 01:14:19 -0800

    Put everything in cogs now. Other than that should be same functionality.

--- botFiles/assignmentBot.py
+++ botFiles/assignmentBot.py
@@ -2,99 +2,93 @@ import discord
 import pyrebase
 import re
 import asyncio
-from config import fireConfig, TG_TOKEN
-from discord.ext import commands, tasks
+from config import fireConfig
+from discord.ext import commands
 from datetime import datetime
 from collections import OrderedDict
 
 firebase = pyrebase.initialize_app(fireConfig)
 db = firebase.database()
-
-bot = commands.Bot(command_prefix='!')
 top = "Assignments"
 
-@bot.event
-async def on_ready():
-	print(f'{bot.user.name} has connected to Discord!')
+def setup(bot):
+    bot.add_cog(AssignmentBot(bot))
 
-#!addAssign "assignment name" mm/dd/yyyy
-@bot.command(name='addAssign', help='Adds a new assignment to the list')
-async def newEvent(ctx, name=None, *date):
-	content = ctx.message.content
-	#check for proper name format
-	if name and (content.count('\"') == 2):
-		title = content.split('"')[1].strip()
-		#strip out mm/dd/yyyy format from following 
-		dateFound = re.search('(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}' , content[content.rindex('"')+1:].strip())
-		#check date format, if date at all, if empty quotes
-		if dateFound and title:
-			#check if assignment name already exists in db
-			if db.child(ctx.guild.id).child(top).child(title).shallow().get().val():
-				await ctx.send('Assignment "' + name + '" already exists!')
+class AssignmentBot(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	#!addAssign "assignment name" mm/dd/yyyy
+	@commands.command(name='addAssign', help='Adds a new assignment to the list')
+	async def newEvent(self, ctx, name=None, *date):
+		content = ctx.message.content
+		#check for proper name format
+		if name and (content.count('\"') == 2):
+			title = content.split('"')[1].strip()
+			#strip out mm/dd/yyyy format from following 
+			dateFound = re.search('(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}' , content[content.rindex('"')+1:].strip())
+			#check date format, if date at all, if empty quotes
+			if dateFound and title:
+				#check if assignment name already exists in db
+				if db.child(ctx.guild.id).child(top).child(title).shallow().get().val():
+					await ctx.send('Assignment "' + name + '" already exists!')
+				else:
+					db.child(ctx.guild.id).child(top).child(title).child('title').set(title)
+					db.child(ctx.guild.id).child(top).child(title).child('date').set(''.join(str(dateFound.group())))
+					if(len(db.child(ctx.guild.id).child(top).get().val()) == 1):
+						self.bot.loop.create_task(self.checkSchedule(ctx.guild))
+					await ctx.send('Added assignment "' + name + '"')
 			else:
-				db.child(ctx.guild.id).child(top).child(title).child('title').set(title)
-				db.child(ctx.guild.id).child(top).child(title).child('date').set(''.join(str(dateFound.group())))
-				if(len(db.child(ctx.guild.id).child(top).get().val()) == 1):
-					bot.loop.create_task(checkSchedule(ctx.guild))
-				await ctx.send('Added assignment "' + name + '"')
+				await ctx.send('Please use the format: !addAssign "Assignment Name" mm/dd/yyyy')
 		else:
 			await ctx.send('Please use the format: !addAssign "Assignment Name" mm/dd/yyyy')
-	else:
-		await ctx.send('Please use the format: !addAssign "Assignment Name" mm/dd/yyyy')
 
-#!delAssign "assignment name"
-@bot.command(name='delAssign', help='Deletes an assignment from the list')
-async def delEvent(ctx, name=None):
-	content = ctx.message.content
-	#check empty argument and in quotes
-	if name and (content.count('\"') == 2):
-		titleDel = content.split('"')[1].strip()
-		#check empty quotes
-		if titleDel:
-			#check exists then removes from db
-			if db.child(ctx.guild.id).child(top).child(titleDel).shallow().get().val():
-				db.child(ctx.guild.id).child(top).child(titleDel).remove()
-				await ctx.send('Removed assignment "' + titleDel + '"')
+	#!delAssign "assignment name"
+	@commands.command(name='delAssign', help='Deletes an assignment from the list')
+	async def delEvent(self, ctx, name=None):
+		content = ctx.message.content
+		#check empty argument and in quotes
+		if name and (content.count('\"') == 2):
+			titleDel = content.split('"')[1].strip()
+			#check empty quotes
+			if titleDel:
+				#check exists then removes from db
+				if db.child(ctx.guild.id).child(top).child(titleDel).shallow().get().val():
+					db.child(ctx.guild.id).child(top).child(titleDel).remove()
+					await ctx.send('Removed assignment "' + titleDel + '"')
+				else:
+					await ctx.send('Assignment "' + name + '" does not exist')
 			else:
-				await ctx.send('Assignment "' + name + '" does not exist')
+				await ctx.send('Please use the format: !delAssign "Assignment Name"')
 		else:
-			await ctx.send('Please use the format: !delAssign "Assignment Name"')
-	else:
-			await ctx.send('Please use the format: !delAssign "Assignment Name"')
+				await ctx.send('Please use the format: !delAssign "Assignment Name"')
 
-#!listAssign
-@bot.command(name='listAssign', help='Lists all assignments')
-async def listEvents(ctx):
-	assignDict = dict(db.child(ctx.guild.id).child(top).get().val())
-	assignDict = dict(sorted(assignDict.items(), key=lambda k_v: datetime.strptime(k_v[1]['date'], '%m/%d/%Y')))
-	if assignDict:
-		for assKey in assignDict:
-			await ctx.send(assignDict[assKey]['title'] + ' due on ' + assignDict[assKey]['date'])
-	else:
-		await ctx.send('There are no assignments to list!')
-
-@bot.command(name='s',help='Shuts down the script')
-async def shutdown(ctx):
-	await ctx.send('Shutting down AssignmentBot')
-	await ctx.bot.logout()
-
-async def checkSchedule(guild):
-	while(True):
-		# sanity check
-		if db.child(guild.id).child(top).get().val():
-			assignDict = dict(db.child(guild.id).child(top).get().val())
+	#!listAssign
+	@commands.command(name='listAssign', help='Lists all assignments')
+	async def listEvents(self, ctx):
+		assignDict = dict(db.child(ctx.guild.id).child(top).get().val())
+		assignDict = dict(sorted(assignDict.items(), key=lambda k_v: datetime.strptime(k_v[1]['date'], '%m/%d/%Y')))
+		if assignDict:
 			for assKey in assignDict:
-				date = datetime.strptime(assignDict[assKey]['date'], '%m/%d/%Y')
-				currDateTime = datetime.today()
-				channel = discord.utils.get(guild.text_channels, name="general")
-				if date.date() == currDateTime.date():
-					if channel:
-						await channel.send("@everyone, " + assignDict[assKey]['title'] + " is due today.")
-				elif date.date() < currDateTime.date():
-					if channel:
-						await channel.send("@everyone, " + assignDict[assKey]['title'] + " was due.")
-						db.child(guild.id).child(top).child(assKey).remove()
-		await asyncio.sleep(28800)
+				await ctx.send(assignDict[assKey]['title'] + ' due on ' + assignDict[assKey]['date'])
+		else:
+			await ctx.send('There are no assignments to list!')
 
 
-bot.run(TG_TOKEN)
+	async def checkSchedule(self, guild):
+		while(True):
+			# sanity check
+			if db.child(guild.id).child(top).get().val():
+				assignDict = dict(db.child(guild.id).child(top).get().val())
+				for assKey in assignDict:
+					date = datetime.strptime(assignDict[assKey]['date'], '%m/%d/%Y')
+					currDateTime = datetime.today()
+					channel = discord.utils.get(guild.text_channels, name="general")
+					if date.date() == currDateTime.date():
+						if channel:
+							await channel.send("@everyone, " + assignDict[assKey]['title'] + " is due today.")
+					elif date.date() < currDateTime.date():
+						if channel:
+							await channel.send("@everyone, " + assignDict[assKey]['title'] + " was due.")
+							db.child(guild.id).child(top).child(assKey).remove()
+			await asyncio.sleep(28800)
--- botFiles/ruleBot.py
+++ botFiles/ruleBot.py
@@ -4,110 +4,101 @@ import pyrebase
 import json
 from urllib import request
 from discord.ext import commands
-from config import fireConfig, TG_TOKEN
+from config import fireConfig
 
 firebase = pyrebase.initialize_app(fireConfig)
 db = firebase.database()
-bot = commands.Bot(command_prefix="!")
 
 FOLDER_STR = 'Rules'
 DEFAULT_RULES_NAME = 'DefaultRules'
 
-@bot.event
-async def on_ready():
-    if not db.child(DEFAULT_RULES_NAME).get().val():
-        with open('defaultRules.json') as j:
-            data = json.load(j)
-            db.child(DEFAULT_RULES_NAME).set(data)
-    print("Bot is online.")
+# bot functions. the '@' line is important where it is used.
+def setup(bot):
+    bot.add_cog(RuleBot(bot))
 
-@bot.command(aliases = ['rules', 'Rule', 'Rules'])
-async def rule(ctx, a = None):
-    if not db.child(ctx.guild.id).child(FOLDER_STR).get().val():
-        ruleDict = default_Dict()
-    else:
-        ruleDict = {k: v for k, v in enumerate(db.child(ctx.guild.id).child(FOLDER_STR).get().val())}
-    if not a:
-        for key in ruleDict:
-            await ctx.send(str(key) + ". "+ ruleDict[key]["title"] + ": " + ruleDict[key]["desc"])
-    else:
-        if not a.isnumeric():
-            await ctx.send("Error: Not a valid rule number." + '\n' + "Please enter a non-negative number to view a specific rule, or no number to view all rules.")
-        elif int(a) >= len(ruleDict):
-            await ctx.send("Error: We do not have that many rules!")
-        else:
-            await ctx.send(a + ". "+ ruleDict[int(a)]["title"] + ": " + ruleDict[int(a)]["desc"]) 
+class RuleBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-@bot.command(aliases = ['addRule', 'addrule'])
-async def add_rule(ctx, *, args = None): 
-    content = ctx.message.content
-    newTitle = "**" + content.split('"')[1].strip() + "**"
-    newDesc = content[content.rindex('"')+1:].strip()
-    
-    if not args:
-        await ctx.send("Please enter a rule title and rule description with the format:\n!addrule \"title\" description")
-    elif content.count('\"') != 2 or args[0] != '"':
-        await ctx.send("Please enclose only the rule title in parentheses. There must be two parentheses in a command. The format to add a new rule is:\n!addrule \"title\" description")
-    elif args[0] == '"' and args[1] == '"':
-        await ctx.send("The rule must have a title. Please use the following format to add a new rule:\n!addrule \"title\" description")
-    else:
+    @commands.command(aliases = ['rules', 'Rule', 'Rules'])
+    async def rule(self, ctx, a = None):
         if not db.child(ctx.guild.id).child(FOLDER_STR).get().val():
-            defDict = default_Dict()
-            db.child(ctx.guild.id).child(FOLDER_STR).update(defDict)
-        index = len(db.child(ctx.guild.id).child(FOLDER_STR).get().val())
-        db.child(ctx.guild.id).child(FOLDER_STR).child(index).child("title").set(newTitle)
-        db.child(ctx.guild.id).child(FOLDER_STR).child(index).child("desc").set(newDesc)
-        await ctx.send("Rule added successfully!")
-
-@bot.command(aliases = ['deleteRule', 'deleterule'])
-async def delete_rule(ctx, a = None):
-    if not a:
-        await ctx.send("Please enter a rule number to delete. If you do not know the number for a rule, enter !rules to see all the rules with their numbers.")
-    elif not a.isnumeric():
-        await ctx.send("Please enter a rule number to delete.")
-    elif int(a) == 0:
-        await ctx.send("Cannot delete rule 0: the welcome message.")
-    elif int(a) >= len(db.child(ctx.guild.id).child(FOLDER_STR).get().val()):
-        await ctx.send("Invalid rule number.")
-    else:
-        delDict = {k: v for k, v in enumerate(db.child(ctx.guild.id).child(FOLDER_STR).get().val())}
-        if int(a) == len(db.child(ctx.guild.id).child(FOLDER_STR).get().val())-1:
-            del delDict[int(a)]
+            ruleDict = self.default_Dict()
         else:
-            for i in range(int(a), len(delDict)-1):
-                delDict[i] = delDict.pop(i+1)
-        db.child(ctx.guild.id).child(FOLDER_STR).set(delDict)
-        await ctx.send("Rule deleted successfully.")
+            ruleDict = {k: v for k, v in enumerate(db.child(ctx.guild.id).child(FOLDER_STR).get().val())}
+        if not a:
+            for key in ruleDict:
+                await ctx.send(str(key) + ". "+ ruleDict[key]["title"] + ": " + ruleDict[key]["desc"])
+        else:
+            if not a.isnumeric():
+                await ctx.send("Error: Not a valid rule number." + '\n' + "Please enter a non-negative number to view a specific rule, or no number to view all rules.")
+            elif int(a) >= len(ruleDict):
+                await ctx.send("Error: We do not have that many rules!")
+            else:
+                await ctx.send(a + ". "+ ruleDict[int(a)]["title"] + ": " + ruleDict[int(a)]["desc"]) 
 
-@bot.command(aliases = ['swaprule', 'swapRule', 'swaprules', 'swapRules'])
-async def swap_rule(ctx, a = None, b = None):            
-    
-    length = len(db.child(ctx.guild.id).child(FOLDER_STR).get().val())
-    if not a or not b:
-        await ctx.send("Error: Please enter two rule numbers to swap.")
-    elif not a.isnumeric():
-        await ctx.send("Error: First value to swap is not numeric.")
-    elif not b.isnumeric:
-        await ctx.send("Error: Second value to swap is not numeric.")
-    elif int(a) == 0 or int(b) == 0:
-        await ctx.send("Error: Cannot swap rule 0: the welcome message")
-    elif int(a) >= length:
-        await ctx.send("Error: First value is too big! We don't have that many rules")
-    elif int(b) >= length:
-        await ctx.send("Error: Second value is too big! We don't have that many rules")
-    else:
-        swapDict = {k: v for k, v in enumerate(db.child(ctx.guild.id).child(FOLDER_STR).get().val())}
-        swapDict[int(a)], swapDict[int(b)] = swapDict[int(b)], swapDict[int(a)]
-        db.child(ctx.guild.id).child(FOLDER_STR).set(swapDict)
-        await ctx.send("Rules swapped successfully")
+    @commands.command(aliases = ['addRule', 'addrule'])
+    async def add_rule(self, ctx, *, args = None): 
+        content = ctx.message.content
+        newTitle = "**" + content.split('"')[1].strip() + "**"
+        newDesc = content[content.rindex('"')+1:].strip()
+        
+        if not args:
+            await ctx.send("Please enter a rule title and rule description with the format:\n!addrule \"title\" description")
+        elif content.count('\"') != 2 or args[0] != '"':
+            await ctx.send("Please enclose only the rule title in parentheses. There must be two parentheses in a command. The format to add a new rule is:\n!addrule \"title\" description")
+        elif args[0] == '"' and args[1] == '"':
+            await ctx.send("The rule must have a title. Please use the following format to add a new rule:\n!addrule \"title\" description")
+        else:
+            if not db.child(ctx.guild.id).child(FOLDER_STR).get().val():
+                defDict = self.default_Dict()
+                db.child(ctx.guild.id).child(FOLDER_STR).update(defDict)
+            index = len(db.child(ctx.guild.id).child(FOLDER_STR).get().val())
+            db.child(ctx.guild.id).child(FOLDER_STR).child(index).child("title").set(newTitle)
+            db.child(ctx.guild.id).child(FOLDER_STR).child(index).child("desc").set(newDesc)
+            await ctx.send("Rule added successfully!")
 
+    @commands.command(aliases = ['deleteRule', 'deleterule'])
+    async def delete_rule(self, ctx, a = None):
+        if not a:
+            await ctx.send("Please enter a rule number to delete. If you do not know the number for a rule, enter !rules to see all the rules with their numbers.")
+        elif not a.isnumeric():
+            await ctx.send("Please enter a rule number to delete.")
+        elif int(a) == 0:
+            await ctx.send("Cannot delete rule 0: the welcome message.")
+        elif int(a) >= len(db.child(ctx.guild.id).child(FOLDER_STR).get().val()):
+            await ctx.send("Invalid rule number.")
+        else:
+            delDict = {k: v for k, v in enumerate(db.child(ctx.guild.id).child(FOLDER_STR).get().val())}
+            if int(a) == len(db.child(ctx.guild.id).child(FOLDER_STR).get().val())-1:
+                del delDict[int(a)]
+            else:
+                for i in range(int(a), len(delDict)-1):
+                    delDict[i] = delDict.pop(i+1)
+            db.child(ctx.guild.id).child(FOLDER_STR).set(delDict)
+            await ctx.send("Rule deleted successfully.")
 
-def default_Dict():
-    return {k: v for k, v in enumerate(db.child(DEFAULT_RULES_NAME).get().val())}
+    @commands.command(aliases = ['swaprule', 'swapRule', 'swaprules', 'swapRules'])
+    async def swap_rule(self, ctx, a = None, b = None):            
+        
+        length = len(db.child(ctx.guild.id).child(FOLDER_STR).get().val())
+        if not a or not b:
+            await ctx.send("Error: Please enter two rule numbers to swap.")
+        elif not a.isnumeric():
+            await ctx.send("Error: First value to swap is not numeric.")
+        elif not b.isnumeric:
+            await ctx.send("Error: Second value to swap is not numeric.")
+        elif int(a) == 0 or int(b) == 0:
+            await ctx.send("Error: Cannot swap rule 0: the welcome message")
+        elif int(a) >= length:
+            await ctx.send("Error: First value is too big! We don't have that many rules")
+        elif int(b) >= length:
+            await ctx.send("Error: Second value is too big! We don't have that many rules")
+        else:
+            swapDict = {k: v for k, v in enumerate(db.child(ctx.guild.id).child(FOLDER_STR).get().val())}
+            swapDict[int(a)], swapDict[int(b)] = swapDict[int(b)], swapDict[int(a)]
+            db.child(ctx.guild.id).child(FOLDER_STR).set(swapDict)
+            await ctx.send("Rules swapped successfully")
 
-@bot.command(name='s',help='Shuts down the script')
-async def shutdown(ctx):
-    await ctx.send('Shutting down TG Bot')
-    await ctx.bot.logout()
-
-bot.run(TG_TOKEN)
+    def default_Dict(self):
+        return {k: v for k, v in enumerate(db.child(DEFAULT_RULES_NAME).get().val())}
