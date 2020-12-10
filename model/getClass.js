e24b35303eaf1e4a0474fa344bac8ac6a6d18312
Dagda8 <andrew-erwin@hotmail.com>
2020-12-09 20:19:26 -0800

    browse-class.html refactored to MVC

---
+++ model/getClass.js
@@ -0,0 +1,33 @@
+const db = require("../server/db_config");
+
+module.exports = async function getClass(className) {
+
+    var classRef = "classes/".concat(className);
+    var ref = db.ref(classRef);
+    var resultsString = { str: "" };
+    var results = {};
+    await ref.once("value", function (snapshot) {
+        snapshot.forEach(function (snapshot) {
+            var info_year = snapshot.child("year").val(); //discord info
+            var info_quarter = snapshot.child("quarter").val(); //discord info
+            var info_profname = snapshot.child("profName").val(); //discord info
+            var info_inviteurl = snapshot.child("inviteURL").val(); //discord info
+            if (info_quarter === "") {
+                return;
+            }
+            var item = info_quarter.concat(" ", info_year);
+            // if Quarter Year is not already in the dict, add it
+            if (!(item in results)) {
+                results[item] = [];
+            }
+
+            // Add to list associated with Quarter Year
+            // results = Dictionary where
+            // { Fall 2020 : [ {prof: ... , discord : ... } , {...} ] }
+            var profDiscordInfo = { "prof": info_profname, "discord": info_inviteurl };
+            results[item].push(profDiscordInfo);
+        });
+    });
+
+    return results;
+}
--- routes/searchController.js
+++ routes/searchController.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // input imports + router post/get here
+const getClass = require('../model/getClass');
 
+router.get('/getClass', (req, res) => {
+    getClass(req.query.className)
+        .then(doc => res.send(doc))
+});
 // end
 
 module.exports = router;
